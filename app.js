f5af0bcfbed78462965ad41e1cb190eb55b6e698
Minh Hoang <anhminh10a2hoa@gmail.com>
2021-11-28 18:35:12 +0200

    test

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var axios = require('axios');
+var cors = require('cors');
+var app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5500',
+  credentials: true,
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+app.get('/',cors(), async(req, res) => {
+  const data = await axios.get('http://www.cc.puv.fi/~asa/json/project.json')
+  console.log(data.data)
+  res.status(200).send(data.data)
+})
+
+app.listen(3000);
