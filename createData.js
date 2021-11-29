71e46726b637b836bc7b3cf214e4cb721511bfd7
Anh <anhminh10a2hoa@gmail.com>
2021-11-29 15:42:07 +0200

    added script for posting the data to mongo db

---
+++ createData.js
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+async function getData(url) {
+  try {
+    const res = await axios.get(url)
+    return res.data
+  } catch (error) {
+    console.log("error", error);
+    return []
+  }
+}
+
+async function addDataToMongo() {
+  const data = await getData('https://node-api-deploy-vamkk.herokuapp.com/project')
+  for(let i = 0; i < data.length; i++) {
+    let newData = data[i]
+    newData.totalprice = data[i].totalprice * 1
+    for(let j = 0; j < data[i].products.length; j++) {
+      newData.products[j].qty = data[i].products[j].qty * 1
+      newData.products[j].unit_price = data[i].products[j].unit_price * 1
+    }
+    await axios.post('https://orders-testing-api.herokuapp.com/api/v1/orders', newData)
+  }
+}
+
+addDataToMongo()
