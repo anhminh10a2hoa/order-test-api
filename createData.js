a6d9b1a47d3f43c78fc6bb09f49ae3c47f8eca24
Anh <anhminh10a2hoa@gmail.com>
2021-11-29 16:00:08 +0200

    test

--- createData.js
+++ createData.js
@@ -14,20 +14,13 @@ async function addDataToMongo() {
   const data = await getData('https://node-api-deploy-vamkk.herokuapp.com/project')
   let i = 0;
 
-  let addDataIntervalTime = setInterval(async function(){
-    i++;
-    if(i == data.length) clearInterval(addDataIntervalTime)
-    else {
-      let newData = data[i]
-      newData.totalprice = data[i].totalprice * 1
-      for(let j = 0; j < data[i].products.length; j++) {
-        newData.products[j].qty = data[i].products[j].qty * 1
-        newData.products[j].unit_price = data[i].products[j].unit_price * 1
-      }
-      const res = await axios.post('https://orders-testing-api.herokuapp.com/api/v1/orders', newData)
-      console.log(res)
-    }
-  }, 2000) 
+  let newData = data[i]
+  newData.totalprice = data[i].totalprice * 1
+  for(let j = 0; j < data[i].products.length; j++) {
+    newData.products[j].qty = data[i].products[j].qty * 1
+    newData.products[j].unit_price = data[i].products[j].unit_price * 1
+  }
+  await axios.post('https://orders-testing-api.herokuapp.com/api/v1/orders', newData)
 }
 
 addDataToMongo()
